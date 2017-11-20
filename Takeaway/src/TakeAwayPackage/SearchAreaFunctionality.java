8c1364498692d1bc831690d9808177c90c1a6ce9
jeffersonsimon <jeffersontrsimonj@gmail.com>
2017-11-20 12:52:20 +0100

    Second Commit

--- Takeaway/src/TakeAwayPackage/SearchAreaFunctionality.java
+++ Takeaway/src/TakeAwayPackage/SearchAreaFunctionality.java
@@ -2,21 +2,29 @@ package TakeAwayPackage;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class SearchAreaFunctionality {
   @Test
   public void f() {
 	  
-	  
+	  System.out.println("Test");
 	  
   }
   @BeforeTest
   public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://www.lieferando.de/");
+	  Assert.assertTrue(true, "The website is opened");
   }
 
   @AfterTest
   public void afterTest() {
+	  System.out.println("AfterTest");
   }
 
 }
