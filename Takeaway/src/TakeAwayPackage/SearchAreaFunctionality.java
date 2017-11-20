87b9542936d2eed947065b822cd339f3e6f776df
jeffersonsimon <jeffersontrsimonj@gmail.com>
2017-11-20 11:28:13 +0100

    first commit

---
+++ Takeaway/src/TakeAwayPackage/SearchAreaFunctionality.java
@@ -0,0 +1,22 @@
+package TakeAwayPackage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class SearchAreaFunctionality {
+  @Test
+  public void f() {
+	  
+	  
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
